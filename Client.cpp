9eeb375cc4b874a8f2e8c089dcc75513ec68fd96
maazey <sushvlab@gmail.com>
2025-11-16 19:02:59 +0300

    Created a method for receiving a responce from the server

--- Client.cpp
+++ Client.cpp
@@ -1,20 +1,44 @@
-#include "Client.h"
-#include "boost/beast.hpp"
-#include "boost/asio/connect.hpp"
-#include "boost/asio/ip/tcp.hpp"
 #include <iostream>
 
-Client::Client(std::string _address) {
+#include "Client.h"
+
+Client::Client(std::string _address, std::string _port, std::string _target_address) {
 	if (!_address.empty()) {
 		address = _address;
 	}
-	else {
-		std::cerr << "address can't be empty!" << std::endl;
+	if (!_port.empty()) {
+		port = _port;
 	}
+	if (!_target_address.empty()) {
+		target_address = _target_address;
+	}
+	
 }
 
-//std::string Client::getResponse() {
-//	namespace asio = boost::asio;
-//	
-//
-//}
+void Client::setConnection() {
+	// Initializing necessary variables //
+	asio::io_context ioc;
+	tcp::resolver resolver{ ioc };
+	tcp::socket stream{ ioc };
+	const auto host = resolver.resolve(address,port);
+
+	// Connecting to the received point //
+
+	asio::connect(stream, host.begin(), host.end());
+
+	// Creating http request //
+
+	http::request<http::string_body> req{ http::verb::get, target_address, 11 };
+	req.set(http::field::host, address);
+	req.set(http::field::user_agent, beast::string_view{ BOOST_BEAST_VERSION_STRING });
+
+	http::write(stream, req);
+
+	beast::flat_buffer buff;
+	http::response<http::string_body> res;
+
+	http::read(stream, buff, res);
+	std::cout << res.body() << std::endl;
+	
+
+}
--- Client.h
+++ Client.h
@@ -1,10 +1,22 @@
 #pragma once
 #include <string>
+#include "boost/asio/ip/tcp.hpp"
+#include "boost/beast/core.hpp"
+#include "boost/beast/http.hpp"
+#include "boost/asio/connect.hpp"
+#include "boost/beast/version.hpp"
+
+namespace beast = boost::beast;
+namespace asio = boost::asio;
+namespace http = boost::beast::http;
+using tcp = asio::ip::tcp;
 
 class Client {
+	std::string port;
 	std::string address;
-
+	std::string target_address; // address after '/'. example - "google.com/test", "test" is a target address //
 public:
-	Client(std::string _address);
-	std::string getResponse();
+	Client(std::string _address, std::string _port, std::string _target_address);
+	void setConnection();
+	
 };
